48320475c95d3d6120213d3b7dc04449870d78c3
Dustin Sugasa <dustin.sugasa@gmail.com>
2019-01-21 14:19:14 +0000

    adding some data gathering

--- growth_inflation/growth_inflation.py
+++ growth_inflation/growth_inflation.py
@@ -14,7 +14,7 @@ import datetime #for dates
 from datetime import datetime
 import quandl #for data
 from math import sqrt
-import tia.bbg.datamgr as dm
+from tia.bbg import LocalTerminal
 #import plotly
 import plotly.plotly as py #for plotting
 import plotly.offline as offline
@@ -25,3 +25,21 @@ from fredapi import Fred
 import credentials
 
 fred = credentials.fred
+
+#set the script start time
+start_time = datetime.now()
+date_now =  "{:%m_%d_%Y}".format(datetime.now())
+
+start_date = '01/01/1948'
+end_date = "{:%m/%d/%Y}".format(datetime.now())
+IDs = ['GDP CYOY Index']
+fields = ['LAST PRICE']
+
+df = LocalTerminal.get_historical(IDs, fields, start_date, end_date, period = 'QUARTERLY',
+                                         non_trading_day_fill_option = 'ALL_CALENDAR_DAYS',
+                                         non_trading_day_fill_method = 'PREVIOUS_VALUE').as_frame()
+df.columns = df.columns.droplevel(-1)
+#df = df.dropna()
+
+
+#d_fx.columns = d_fx.columns.droplevel(-1)
